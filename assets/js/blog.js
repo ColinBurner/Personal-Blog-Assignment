491fa17f4fb33b4a0bbc23798510757665268d69
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-05-19 20:44:15 -0500

    added comments on CSS and JavaScript files

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,4 @@
+// toggle theme function controls what happens when the dark/light mode button is clicked based on which one is currently active
 function toggleTheme() {
   const currentTheme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
@@ -7,7 +8,7 @@ function toggleTheme() {
   updateThemeToggleButton(newTheme);
 }
 
-
+// initialize theme function switches to the stored theme to make the button consistently interactive
 function initializeTheme() {
   const storedTheme = localStorage.getItem('theme') || 'light';
   document.body.classList.toggle('dark-mode', storedTheme === 'dark');
@@ -15,11 +16,13 @@ function initializeTheme() {
   updateThemeToggleButton(storedTheme);
 }
 
+// this function makes the button emojis of a moon and sun to represent dark and light mode
 function updateThemeToggleButton(theme) {
   const button = document.getElementById('themeToggle');
   button.innerHTML = theme === 'dark' ? '🌞' : '🌙';
 }
 
+// this function gets the input data from the landing page form and displays it in the form of a blog post on the secondary page
 function displayBlogPosts() {
   const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
   const container = document.getElementById('blogPostsContainer');
@@ -33,6 +36,7 @@ function displayBlogPosts() {
   });
 }
 
+// these event listeners listen for the dark/light mode button and the back button to be clicked in order to execute the proper function
 document.getElementById('themeToggle').addEventListener('click', toggleTheme);
 document.getElementById('backButton').addEventListener('click', () => {
   window.location.href = 'index.html';
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,4 @@
+// this function collects user data from the form upon the event of a click on the submit button
 document.getElementById('blogForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -5,6 +6,7 @@ document.getElementById('blogForm').addEventListener('submit', function(event) {
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
 
+// if name, title and content sections are filled out, the data is pushed to local storage
     if (name && title && content) {
         const blogPost = { name: name, title: title, content: content };
         let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
@@ -16,5 +18,5 @@ document.getElementById('blogForm').addEventListener('submit', function(event) {
     }
 });
 
-
+// ^ else alert happens if a section is not filled out
 
